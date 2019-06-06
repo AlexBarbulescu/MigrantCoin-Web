27ca8e9fb7c4d47e6cf1b640ae7ab29bd474ee82
Alex Barbulescu <alex_tnt89@yahoo.com>
2019-06-06 16:45:17 +0300

    Text Update, and Whitepaper

--- index.php
+++ index.php
@@ -489,7 +489,7 @@
                                         <div class="double-bounce2"></div>
                                         <div class="double-bounce3"></div>
                                     </div>
-                                    <h6 class="animation" data-animation="fadeInUp" data-animation-delay="0.3s">Q2 2019</h6>
+                                    <h6 class="animation" data-animation="fadeInUp" data-animation-delay="0.3s">Q2-Q3 2019</h6>
                                     <p class="animation lang" key="ROADMAP_03" data-animation="fadeInUp" data-animation-delay="0.4s">Token creation</p>
                                     <p class="animation lang" key="ROADMAP_04" data-animation="fadeInUp" data-animation-delay="0.4s">Sales</p>
                                 </div>
@@ -922,6 +922,38 @@
                             <img class="img-fluid" src="assets/images/team/00.png" alt="">
                             <div class="info-top">
                                 <div class="name">
+                                    Vlad Cosma
+                                </div>
+                                <div class="job">
+
+                                </div>
+                            </div>
+                            <div class="info-bottom">
+                                <div class="name">
+                                    Vlad Cosma
+                                </div>
+                                <div class="job">
+
+                                </div>
+                                <p>Insert text here</p>
+                                <div class="social">
+                                    <a href="https://www.linkedin.com/in/vlad-cosma-66b272125/" target="_blank">
+                                        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <title>LinkedIn icon</title>
+                                            <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.6s">
+                        <div class="team-item">
+                            <div class="hover-back"></div>
+                            <img class="img-fluid" src="assets/images/team/00.png" alt="">
+                            <div class="info-top">
+                                <div class="name">
                                     Șerban Victor
                                 </div>
                                 <div class="job lang" key="TEAM_06">
@@ -948,7 +980,7 @@
                         </div>
                     </div>
 
-                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.6s">
+                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.7s">
                         <div class="team-item">
                             <div class="hover-back"></div>
                             <img class="img-fluid" src="assets/images/team/00.png" alt="">
@@ -980,7 +1012,7 @@
                         </div>
                     </div>
 
-                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.7s">
+                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.8s">
                         <div class="team-item">
                             <div class="hover-back"></div>
                             <img class="img-fluid" src="assets/images/team/00.png" alt="">
@@ -1012,7 +1044,7 @@
                         </div>
                     </div>
 
-                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.8s">
+                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.9s">
                         <div class="team-item">
                             <div class="hover-back"></div>
                             <img class="img-fluid" src="assets/images/team/05.png" alt="">
@@ -1044,7 +1076,7 @@
                         </div>
                     </div>
 
-                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.8s">
+                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="1.0s">
                         <div class="team-item">
                             <div class="hover-back"></div>
                             <img class="img-fluid" src="assets/images/team/04.png" alt="">
@@ -1076,7 +1108,7 @@
                         </div>
                     </div>
 
-                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.8s">
+                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="1.1s">
                         <div class="team-item">
                             <div class="hover-back"></div>
                             <img class="img-fluid" src="assets/images/team/06.png" alt="">
@@ -1100,7 +1132,7 @@
                         </div>
                     </div>
 
-                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="1.0s">
+                    <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="1.2s">
                         <div class="team-item">
                             <div class="hover-back"></div>
                             <img class="img-fluid" src="assets/images/team/03.png" alt="">
