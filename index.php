fcd57b015b185df660cb06eee025b405d4c8d9ad
Alex Barbulescu <alex_tnt89@yahoo.com>
2019-07-01 16:00:28 +0300

    Weekly Update

--- index.php
+++ index.php
@@ -133,16 +133,16 @@
                         <li class="animation" data-animation="fadeInDown" data-animation-delay="0.10s">&nbsp;</li>
 
                         <li class="animation" data-animation="fadeInDown" data-animation-delay="0.11s">
-                            <a class="nav-icon nav_icon nav_item" href="https://www.facebook.com/Migrantcoin-2336469606669909" target="_blank"><i class="fab fa-facebook"></i></a>
+                            <a class="nav-icon nav_icon nav_item" href="https://www.facebook.com/MigrantCoinOfficial/" target="_blank"><i class="fab fa-facebook"></i></a>
                         </li>
                         <li class="animation" data-animation="fadeInDown" data-animation-delay="0.12s">
                             <a class="nav-icon nav_icon nav_item" href="https://t.me/migrantcoin" target="_blank"><i class="fab fa-telegram-plane"></i></a>
                         </li>
-                        <li class="animation" data-animation="fadeInDown" data-animation-delay="0.13s">
-                            <a class="nav-icon nav_icon nav_item" href="https://medium.com/@Migrantcoin" target="_blank"><i class="fab fa-medium-m"></i></a>
-                        </li>
                         <li class="animation" data-animation="fadeInDown" data-animation-delay="0.14s">
                             <a class="nav-icon nav_icon nav_item" href="https://twitter.com/MigrantcoinO" target="_blank"><i class="fab fa-twitter"></i></a>
+                        </li>
+                        <li class="animation" data-animation="fadeInDown" data-animation-delay="0.13s">
+                            <a class="nav-icon nav_icon nav_item" href="https://medium.com/@Migrantcoin" target="_blank"><i class="fab fa-medium-m"></i></a>
                         </li>
                         <li class="animation" data-animation="fadeInDown" data-animation-delay="0.15s">
                             <a class="nav-icon nav_icon nav_item" href="https://www.instagram.com/migrantcoin/" target="_blank"><i class="fab fa-instagram"></i></a>
@@ -890,6 +890,38 @@
             </div>
             <div class="row small_space justify-content-center">
 
+                <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.7s">
+                    <div class="team-item">
+                        <div class="hover-back"></div>
+                        <img class="img-fluid" src="assets/images/team/00.jpg" alt="Team Member">
+                        <div class="info-top">
+                            <div class="name">
+                                Vlad Cosma
+                            </div>
+                            <div class="job lang" key="TEAM_15">
+                                <?php print_r($language[$active_language]['TEAM_15']); ?>
+                            </div>
+                        </div>
+                        <div class="info-bottom">
+                            <div class="name">
+                                Vlad Cosma
+                            </div>
+                            <div class="job lang" key="TEAM_15">
+                                <?php print_r($language[$active_language]['TEAM_15']); ?>
+                            </div>
+                            <p></p>
+                            <div class="social">
+                                <a href="https://www.linkedin.com/in/vlad-cosma-66b272125/" target="_blank">
+                                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <title>LinkedIn icon</title>
+                                        <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
 
                 <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.3s">
                     <div class="team-item">
@@ -1009,38 +1041,6 @@
                             <p></p>
                             <div class="social">
                                 <a href="https://www.linkedin.com/in/istvanpreda/" target="_blank">
-                                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <title>LinkedIn icon</title>
-                                        <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path>
-                                    </svg>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-6 col-lg-3 col-md-4 col-sm-4 animation" data-animation="fadeInDown" data-animation-delay="0.7s">
-                    <div class="team-item">
-                        <div class="hover-back"></div>
-                        <img class="img-fluid" src="assets/images/team/00.jpg" alt="Team Member">
-                        <div class="info-top">
-                            <div class="name">
-                                Vlad Cosma
-                            </div>
-                            <div class="job">
-
-                            </div>
-                        </div>
-                        <div class="info-bottom">
-                            <div class="name">
-                                Vlad Cosma
-                            </div>
-                            <div class="job">
-
-                            </div>
-                            <p></p>
-                            <div class="social">
-                                <a href="https://www.linkedin.com/in/vlad-cosma-66b272125/" target="_blank">
                                     <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <title>LinkedIn icon</title>
                                         <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path>
